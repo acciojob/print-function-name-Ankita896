5977d929653d0b98c8a314612e0862d2bd0ddac0
Ankita896 <ankita143ankyy@gmail.com>
2025-08-03 16:21:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function printFunctionName(func) {
+    func();
+}
+
+function functionName() {
+    alert(arguments.callee.name);
+}
+
+printFunctionName(functionName);
